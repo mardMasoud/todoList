8203f2a1a30f733242c90c73f1569151b2fa1df6
masoudMarde <mardmasoud66@gmail.com>
2023-04-22 21:15:44 +0430

    final_todo

--- js/script.js
+++ js/script.js
@@ -1,29 +1,50 @@
 ﻿let todoList = document.querySelector(".list-group");
-let inputElm = document.querySelector(".form-control");
+let inputElm = document.querySelector("input");
 const alertElm = document.querySelector(".alert");
 const formElm = document.querySelector("form");
-let iconDel;
+let iElms = document.querySelectorAll("i");
 let liElm;
 formElm.addEventListener("submit", function (e) {
   e.preventDefault();
 });
+
+
+iElms.forEach(function (ic) {
+  ic.addEventListener("click", function () {
+    ic.parentElement.remove();
+  });
+});
+function addTodo(){
+
+}
 inputElm.addEventListener("keydown", function (e) {
-  if (e.key === "Enter") {
-    if (e.target.value != "") {
+  alertElm.innerHTML=''
+  let inputValue=e.target.value
+ // e.target.value=''
+  if (e.keyCode === 13) {
+    e.target.value=''
+   
+    if (inputValue) {
       liElm = document.createElement("li");
-      liElm.classList.add(
-        "list-group-item",
-        "d-flex",
-        "justify-content-between",
-        "align-items-center"
-      );
+      liElm.className =
+        "list-group-item d-flex justify-content-between align-items-center";
+
       let spanElm = document.createElement("span");
-      spanElm.textContent = e.target.value;
-      liElm.append(spanElm);
-      iconDel = document.createElement("i");
-      iconDel.classList.add("fa", "fa-trash-o", "delete");
-      liElm.append(iconDel);
+      spanElm.textContent = inputValue;
+     
+      let newiconDel = document.createElement("i");
+      newiconDel.className = "fa fa-trash-o delete";
+      liElm.append(spanElm,newiconDel);
+      newiconDel.addEventListener("click", function () {
+        newiconDel.parentElement.remove();
+      });
+      
+      
+
       todoList.append(liElm);
-    } else alertElm.innerHTML = "input is empty";
+     
+    } else {alertElm.innerHTML = "input is empty";
+  
+  }
   }
 });
